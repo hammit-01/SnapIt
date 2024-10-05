6bbea0963d44b700fc2249ac3849714a8f2b1a39
hammit <mpa0530@gmail.com>
2024-10-06 06:46:13 +0900

    간결화

--- app/src/main/java/com/example/snapit/EditProfileActivity.kt
+++ app/src/main/java/com/example/snapit/EditProfileActivity.kt
@@ -3,26 +3,19 @@ package com.example.snapit
 import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
-import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.snapit.Datastore.UserDataClass
 import com.example.snapit.Datastore.UserDataHolder
-import com.example.snapit.Datastore.UserDataHolder.userData
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import org.w3c.dom.Comment
-import java.lang.Exception
 
 class EditProfileActivity : AppCompatActivity() {
     private var auth = FirebaseAuth.getInstance()
--- app/src/main/java/com/example/snapit/UserActivity.kt
+++ app/src/main/java/com/example/snapit/UserActivity.kt
@@ -10,8 +10,6 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.snapit.Adapter.ImageAdapter
-import com.example.snapit.Datastore.UserDataClass
 import com.example.snapit.Datastore.UserDataHolder
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
