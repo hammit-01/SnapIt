017d1bb62db8868e6138ceec2d9c2edac33c9fd9
hammit <mpa0530@gmail.com>
2024-10-06 06:38:55 +0900

    readME 추가, 비디오 플레이어 액티비티 뒤로가기 추가

--- app/src/main/java/com/example/snapit/VideoPlayerActivity.kt
+++ app/src/main/java/com/example/snapit/VideoPlayerActivity.kt
@@ -1,13 +1,16 @@
 package com.example.snapit
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 
 class VideoPlayerActivity : AppCompatActivity() {
     private lateinit var videoView: VideoView
 
+    private lateinit var back: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.video_item)
@@ -20,6 +23,21 @@ class VideoPlayerActivity : AppCompatActivity() {
             videoView.setOnPreparedListener { mediaPlayer ->
                 mediaPlayer.isLooping = false // 반복 재생 안 함
                 videoView.start() // 비디오 시작
+            }
+        }
+
+        // 뒤로가기 버튼
+        back = findViewById(R.id.back)
+        back.setOnClickListener {
+            if (isTaskRoot) {
+                // 현재 액티비티가 스택에서 마지막 액티비티라면 앱을 종료하지 않고 홈으로
+                val intent = Intent(Intent.ACTION_MAIN)
+                intent.addCategory(Intent.CATEGORY_HOME)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+            } else {
+                // 이전 액티비티가 있으면 뒤로가기 동작 실행
+                finish()
             }
         }
     }
